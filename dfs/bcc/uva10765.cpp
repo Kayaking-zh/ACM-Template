7ae37ce0965371734dee374cb6629727f2a3f0ec
kayaking-zh <1738160864@qq.com>
2018-10-11 14:03:57 +0800

    update bcc

--- dfs/bcc/uva10765.cpp
+++ dfs/bcc/uva10765.cpp
@@ -13,11 +13,12 @@
 #define lson rt<<1, l, mid
 #define rson rt<<1|1, mid+1, r
 #define lowbit(x) x&-x
+#define mp make_pair
 
 using namespace std;
 
 const int MOD = 1000000007;
-const int N = 1010;
+const int N = 1000010;
 const int M = 100010;
 const int INT_INF = 0x3f3f3f3f;
 const LL LL_INF = 0x3f3f3f3f3f3f3f3f;
@@ -51,20 +52,34 @@ LL Pow(LL m, LL n) { LL ans = 1; while(n) { if(n&1) ans = ans*m%MOD; n >>= 1; m 
                                                      *
 **/
 
-int n, m, dfs_clock, bcc_cnt;
-int dfn[N], low[N], iscut[N], vis[N], color[N], bccno[N], g[N][N];
+int tot, dfs_clock, bcc_cnt;
+int to[M], nxt[M], head[N], dfn[N], low[N], iscut[N], bccno[N], cnt[N];
 stack<pair<int, int> >stk;
 vector<int>bcc[N];
+struct node{
+    int id, block;
+}f[N];
+
+void init() {
+    tot = dfs_clock = bcc_cnt = 0; memset(head, 0, sizeof(head));
+    memset(dfn, 0, sizeof(dfn)); memset(iscut, 0, sizeof(iscut));
+    memset(bccno, 0, sizeof(bccno)); memset(f, 0, sizeof(f));
+}
+
+void addEdge(int u, int v) {
+    to[++tot] = v, nxt[tot] = head[u], head[u] = tot;
+}
 
 void dfs(int u, int fa = -1) {
     dfn[u] = low[u] = ++dfs_clock;
     int child = 0;
-    for(int i = 1; i <= n; i++) if(g[u][i]) {
-        if(!dfn[i]) {
-            stk.push(make_pair(u, i));
-            dfs(i, u);
-            low[u] = min(low[u], low[i]);
-            if(low[i] >= dfn[u]) {
+    for(int i = head[u]; i; i = nxt[i]) {
+        int v = to[i];
+        if(!dfn[v]) {
+            stk.push(make_pair(u, v)), child++;
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
+            if(low[v] >= dfn[u]) {
                 iscut[u] = true; bcc_cnt++;
                 bcc[bcc_cnt].clear();
                 for(; ; ) {
@@ -75,53 +90,39 @@ void dfs(int u, int fa = -1) {
                     if(bccno[b] != bcc_cnt) {
                         bcc[bcc_cnt].push_back(b); bccno[b] = bcc_cnt;
                     }
-                    if(a == u && b == i) break ;
+                    if(a == u && b == v) break ;
                 }
             }
-        } else if(dfn[i] < dfn[u] && i != fa) {
-            stk.push(make_pair(u, i));
-            low[u] = min(low[u], dfn[i]);
+        } else if(dfn[v] < dfn[u] && v != fa) {
+            stk.push(make_pair(u, v));
+            low[u] = min(low[u], dfn[v]);
         }
     }
     if(fa < 0 && child == 1) iscut[u] = 0;
 }
 
-void init() {
-    dfs_clock = bcc_cnt = 0;
-    for(int i = 1; i <= n; i++) for(int j = i+1; j <= n; j++) g[i][j] = g[j][i] = 1;
-    memset(iscut, 0, sizeof(iscut)); memset(dfn, 0, sizeof(dfn));
-    memset(vis, false, sizeof(vis)); memset(bccno, 0, sizeof(bccno));
-}
-
-bool bipartite(int u, int id) {
-    for(int i = 1; i <= n; i++) if(g[u][i]) {
-        if(bccno[i] != id) continue ;
-        if(color[i] == -1) {
-            color[i] = (color[u]^1);
-            if(!bipartite(i, id)) return false;
-        } else {
-            if(color[u] == color[i]) return false;
-        }
-    }
-    return true;
+int cmp(const node&a, const node&b) {
+    if(a.block == b.block) return a.id < b.id;
+    return a.block > b.block;
 }
 
 int main() {
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    int n, m;
     while(scanf("%d%d", &n, &m) && (n || m)) {
         init();
-        for(int i = 1, k1, k2; i <= m; i++) {
-            scanf("%d%d", &k1, &k2);
-            g[k1][k2] = g[k2][k1] = 0;
+        int u, v;
+        while(scanf("%d%d", &u, &v) && (u!=-1 && v!=-1)) {
+            u++, v++;
+            addEdge(u, v); addEdge(v, u);
         }
         for(int i = 1; i <= n; i++) if(!dfn[i]) dfs(i);
+        for(int i = 1; i <= n; i++) f[i].id = i;
         for(int i = 1; i <= bcc_cnt; i++) {
-            memset(color, -1, sizeof(color));
-            for(int j = 0, lim = bcc[i].size(); j < lim; j++) bccno[bcc[i][j]] = i;
-            int u = bcc[i][0]; color[u] = 1;
-            if(!bipartite(u, i)) for(int j = 0, lim = bcc[i].size(); j < lim; j++) vis[bcc[i][j]] = 1;
+            for(int j = 0, lim = bcc[i].size(); j < lim; j++) f[bcc[i][j]].block++;
         }
-        int ans = n;
-        for(int i = 1; i <= n; i++) ans -= vis[i];
-        printf("%d\n", ans);
+        sort(f+1, f+n+1, cmp);
+        for(int i = 1; i <= m; i++) printf("%d %d\n", f[i].id-1, f[i].block);
     }
 }
